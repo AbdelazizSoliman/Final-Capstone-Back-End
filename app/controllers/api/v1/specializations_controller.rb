d04d72cfffdf617592fdb17b76b67ea04ce492b4
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-10-11 11:03:04 +0100

    Fix Associations relationship

--- app/controllers/api/v1/specializations_controller.rb
+++ app/controllers/api/v1/specializations_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::SpecializationsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_patient!
 
   # GET api/v1/specializations
   def index
